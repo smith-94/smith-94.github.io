e8b3550925d839f2b0b78d41fcb362e2333d3a39
smith-94 <dbtjdwocjswo@gmail.com>
2024-03-25 13:14:24 +0900

    deploy

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"efdde39a2a57d32564b0225ffffa088c5a14fe
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1701055824"
+    serviceWorkerVersion: "2621301431"
   }
 });
