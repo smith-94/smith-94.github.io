ffb2b86b15992ffb3d6cfa513bae73cf39b0d549
smith-94 <dbtjdwocjswo@gmail.com>
2024-03-25 12:01:18 +0900

    deploy

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -6,11 +6,11 @@ Read more: https://developer.mozilla.org/en-US/docs/Web/Security/Secure_Contexts
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"7c139e0a4897fa1e436457afe075ac00df8cadb2","builds":[{"compileTarget":"dart2js","renderer":"auto","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"efdde39a2a57d32564b0225ffffa088c5a14fe75","builds":[{"compileTarget":"dart2js","renderer":"auto","mainJsPath":"main.dart.js"}]};
 
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "403298112"
+    serviceWorkerVersion: "1724150407"
   }
 });
